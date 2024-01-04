c1efcf419fa7c0a433d7dfa43608a0e5aada470c
julydeath <manojkarajada.mk@gmail.com>
2024-01-04 19:33:52 +0530

    chore : changes in server js

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
+  url: env("BACKEND_URL_LOCAL"),
   app: {
     keys: env.array("APP_KEYS", ["key1", "key2"]),
   },
