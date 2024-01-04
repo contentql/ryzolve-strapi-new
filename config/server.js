320a62b100dda06d97ebcf34029945e569a08be3
julydeath <manojkarajada.mk@gmail.com>
2024-01-04 19:45:30 +0530

    chore :  prod error resolve

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("BACKEND_URL_LOCAL"),
+  // url: env("https://ryzolve-strapi-production-ca3d.up.railway.app/"),
   app: {
     keys: env.array("APP_KEYS", ["key1", "key2"]),
   },
