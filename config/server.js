bdc57a46ecf32148668dd7bc5f78006614576beb
julydeath <manojkarajada.mk@gmail.com>
2024-01-04 20:00:48 +0530

    chore : server redirection url

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  // url: env("https://ryzolve-strapi-production-ca3d.up.railway.app/"),
+  url: "https://ryzolve-strapi-production-ca3d.up.railway.app/",
   app: {
     keys: env.array("APP_KEYS", ["key1", "key2"]),
   },
